674e6316fb956d1b4be48035acbdca3f720c2ab4
FrankPZT <lihanyang19940724@gmail.com>
2021-10-30 23:46:14 +1000

    prac finalized

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import { Button } from '@material-ui/core';
-import LoginIcon from '@material-ui/icons/AccountCircle';
-import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
 import { useEffect, useState } from 'react';
-
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import axios from "axios";
+import style from "./App.module.scss"
+import Grid from "@material-ui/core/Grid";
 function App() {
   const [data, setData] = useState([]);
+  // useEffect(() => {
+  //   fetch("https://finalspaceapi.com/api/v0/character/?limit=12")
+  //   .then((res) => res.json())
+  //   .then((data) => setData(data))
+  // }, [])
+
   useEffect(() => {
-    fetch("https://finalspaceapi.com/api/v0/character/?limit=12")
-    .then((res) => res.json())
-    .then((data) => setData(data))
-  }, [])
-  console.log(data);
+    axios.get('https://finalspaceapi.com/api/v0/character/?limit=12')
+    .then(res => setData(res.data)) 
+  },[])
+
+  console.log("not in the effect hook");
   return (
     <>
-      <Button startIcon={ <LoginIcon />}  endIcon={ <AccessAlarmIcon />} size="small" color="primary" variant="contained">
-        Click Me for blessing
-      </Button>
-      <p>
-        
-      </p>
+        <Container center="true" className={style.container}>
+          <Typography color="textPrimary" gutterBottom variant="h1" center="true">
+            Final Space Characters:
+          </Typography>
+            <Grid container spacing={3}>
+              {data.map((character) => {
+                  return<Grid item xs={12} sm={4} key={character.id}>
+                    <div>
+                      <Card className={style.charCard}>
+                        <CardContent>
+                        <Typography color="primary" gutterBottom variant="h5" center="true">
+                          {character.name}
+                        </Typography>
+                        <Typography color="textSecondary" gutterBottom variant="subtitle1" center="true">
+                          {character.name}
+                        </Typography>
+                        </CardContent>
+                        <CardMedia classNmae={style.charCardImg} image={character.img_url} style= {{height: "300px"}}/>
+                      </Card>
+                    </div>
+                  </Grid>
+                })}
+            </Grid>
+        </Container>
     </>
   );
 }
